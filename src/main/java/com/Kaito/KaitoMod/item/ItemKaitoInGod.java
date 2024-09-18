962ec89d97663213997ef01d5acb414bffa431da
root <root@DESKTOP-O3VC0DR.localdomain>
2024-09-18 14:37:27 +0900

    Update : Add item tag

--- src/main/java/com/Kaito/KaitoMod/item/ItemKaitoInGod.java
+++ src/main/java/com/Kaito/KaitoMod/item/ItemKaitoInGod.java
@@ -1,6 +1,14 @@
 package com.Kaito.KaitoMod.item;
 
+import com.Kaito.KaitoMod.regi.KaitoModTags;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 
 public class ItemKaitoInGod extends Item {
     public ItemKaitoInGod() {
@@ -8,5 +16,16 @@ public class ItemKaitoInGod extends Item {
                 .fireResistant()
                 .stacksTo(75)
         );
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
+        ItemStack stack = player.getItemInHand(hand);
+        if(stack.is(KaitoModTags.Items.KAITO_SERIES)){
+            player.addEffect((new MobEffectInstance(MobEffects.GLOWING, 20*60*3, 0)));
+        }else {
+            player.addEffect((new MobEffectInstance(MobEffects.DARKNESS, 20*60*3, 0)));
+        }
+        return InteractionResultHolder.consume(stack);
     }
 }
