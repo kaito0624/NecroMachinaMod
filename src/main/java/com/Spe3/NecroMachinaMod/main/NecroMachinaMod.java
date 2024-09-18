5f8775a5f3c6abaf762fea9aa71c96c4ea80a10e
root <root@DESKTOP-O3VC0DR.localdomain>
2024-09-18 17:52:13 +0900

    Add bunner

---
+++ src/main/java/com/Spe3/NecroMachinaMod/main/NecroMachinaMod.java
@@ -0,0 +1,13 @@
+package com.Spe3.NecroMachinaMod.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("necromachinamod")
+public class NecroMachinaMod {
+
+    public static final String MOD_ID = "necromachinamod";
+
+    public NecroMachinaMod(){
+        
+    }
+}
